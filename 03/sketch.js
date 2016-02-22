b1ec3e2a3aabc7a7d0aec0d42b2833d58a683778
kaitlippe <muchness96@yahoo.com>
2016-02-21 22:10:31 -0500

    move into setup

--- 03/sketch.js
+++ 03/sketch.js
@@ -1,15 +1,16 @@
 function setup() {
   createCanvas(width, height)
   background(255);
-}
 
-function draw() {
-	var count = 0;
+  	var count = 0;
   	var poem = "With a deposit of that size, they could get check-guarantee cards with no waiting period. They went to Bath and opened accounts at Midland Bank under the names Tara Lucy Noe and Christopher Platt Noe. Their new checkbooks came in days, and they rented a flat on Baker Street. As the Holtes, they’d avoided being in the same M. and S. concurrently. This time, Haysom suggested that they go in at the same time but stay apart. At the last M. and S. they visited, on April 30th, a store employee noticed them and grew suspicious. She followed them onto";
  	for (var n = 0; n <= poem.length; n += 1) {
  		if ( n % 4 == 0){
  			console.log("yes")
  		}
  	}
+}
+
+function draw() {
   
 }
