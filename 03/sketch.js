587799854507f0075d5ac4740b4035eec8f14ca5
kaitlippe <muchness96@yahoo.com>
2016-02-22 00:10:24 -0500

    hj

--- 03/sketch.js
+++ 03/sketch.js
@@ -1,5 +1,7 @@
+var array = [2, 4, 8, 16, 8, 4, 2]
 var poem = "With a deposit of that size, they could get check-guarantee cards with no waiting period. They went to Bath and opened accounts at Midland Bank under the names Tara Lucy Noe and Christopher Platt Noe. Their new checkbooks came in days, and they rented a flat on Baker Street. As the Holtes, they’d avoided being in the same M. and S. concurrently. This time, Haysom suggested that they go in at the same time but stay apart. At the last M. and S. they visited, on April 30th, a store employee noticed them and grew suspicious. She followed them onto";
 var poemSplit = poem.split(" ");
+var count = 0;
 
 
 function setup() {
@@ -7,8 +9,9 @@ function setup() {
   background(255);
 
  	for (var n = 0; n <= poemSplit.length; n += 1) {
- 		if ( poemSplit % 4 == 0){
- 			console.log("no")
+ 		if ( n % 4 == 0){
+ 			console.log(poemSplit[add + array[add])
+ 			add++;
  		}
  	}
 }
